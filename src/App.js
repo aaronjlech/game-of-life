e2ec41c518f0e3ad500fd170d6caa231eb7b4508
Aaron LeCheminant <aaronjlech@gmail.com>
2017-10-16 15:15:56 -0400

    setting up game buttons

--- src/App.js
+++ src/App.js
@@ -6,18 +6,28 @@ import Row from "./components/Row";
 class App extends Component {
    state = {
       grid: makeRandomGrid(50, 50),
-      generations: 0
+      generations: 0,
+      runGame: true
    }
    componentDidMount() {
-      this._getNextGrid()
+      if(this.state.runGame){
+         this._getNextGrid()
+      }
    }
+   _toggleGame = () => {
+      console.log('hello!');
+
+      this.setState({runGame: !this.state.runGame})
+   }
+
    _handleClick = (x, y) => {
       const { grid } = this.state;
       grid[x][y] = grid[x][y] ? 0 : 1;
       this.setState({ grid });
    }
    _getNextGrid = () => {
-      this.interval = window.setInterval(() => {
+
+       this.interval = window.setInterval(() => {
          let currentGrid = this.state.grid
          const newGrid = []
          for (let y = 0; y < currentGrid.length; y++) {
@@ -31,17 +41,24 @@ class App extends Component {
                newGrid[y].push(setCellStatus(currentCell, neighbourCount))
             }
          }
-         console.log(newGrid[21][20] === currentGrid[21][20]);
-         this.setState({ grid: newGrid})
+         this.setState({ grid: newGrid, generations: this.state.generations += 1})
       }, 100)
+      if(!this.state.runGame){
+         console.log('hello!');
+         window.clearInterval(this.interval);
+      }
    }
 
 
    render() {
-      const { grid } = this.state;
-      console.log(grid[20][10]);
+      const { grid, generations, runGame } = this.state;
       return (
          <div className="App">
+            <h1>generations: {generations}</h1>
+            <div className="button-container">
+               <button className="reset">reset grid</button>
+               <button className="pause" onClick={this._toggleGame}>{ runGame ? 'pause' : 'start'} generations</button>
+            </div>
             <div className="grid">
                {grid.length &&
                   grid.map((cells, i) => {
